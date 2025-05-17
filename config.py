070a3e4db0c0e4e1e635c21fb8b7ab4bf47f673a
rajeshboy669 <rajeshgautam.1077@gmail.com>
2025-05-17 21:55:04 +0545

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config(object):
       API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
       API_ID = int(getenv("API_ID", "20389440"))
       AS_COPY = True if getenv("AS_COPY", True) == "`{file_name}`" else True
-      BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo")
-      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1001722984461:-1001623633000").replace("\n", " ").split(' '))
+      BOT_TOKEN = getenv("BOT_TOKEN", "6548971110:AAGW5_X3noXCCZQWuXm-FtfDvPfS-tTKe7c")
+      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1001822272222").replace("\n", " ").split(' '))
 
 
 # Don't Remove Credit @VJ_Botz
